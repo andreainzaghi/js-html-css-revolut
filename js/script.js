23ecee335dd7c4d89321f51a4ae42ae36b73f951
Andrea <andrea3aprile99@gmail.com>
2021-03-16 19:53:13 +0100

    s

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
+$(function() {
+  // Modo 1 - all'hover usando i metodi jquery
+  // $('.main-menu-list > li').mouseenter(
+  //   function() {
+  //     $(this).children('.main-menu-dropdown-container').slideDown();
+  //   }
+  // );
+  //
+  // $('.main-menu-list > li').mouseleave(
+  //   function() {
+  //     $(this).children('.main-menu-dropdown-container').slideUp();
+  //   }
+  // );
 
-  $('.list1 li').click(function()
-{
-  $(this).children(".dropdown-menu").slideToggle()}
-);
+  // Modo 2 - all'hover aggiugendo la classe
+  $('.main-menu-list > li').mouseenter(
+    function() {
+      //$(this).siblings('li').children('.main-menu-dropdown-container').removeClass('active');
+      $('.main-menu-dropdown-container').removeClass('active');
+      $(this).children('.main-menu-dropdown-container').addClass('active');
+    }
+  );
+
+  // $('.main-menu-list > li').mouseleave(
+  //   function() {
+  //     $(this).children('.main-menu-dropdown-container').removeClass('active');
+  //   }
+  // );
+
+  // Modo 3 - col click e il toggle sulla classe
+  $('.main-menu-list > li').click(
+    function() {
+      $(this).children('.main-menu-dropdown-container').toggleClass('active');
+    }
+  );
+
+
+});
